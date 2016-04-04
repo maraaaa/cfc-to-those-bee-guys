8acd47b10516d2119997869673dd5613a029e566
maraaaa <mara.arthie@gmail.com>
2016-04-04 13:01:19 -0400

    Initial Comit

---
+++ README.md
@@ -0,0 +1,13 @@
+'CFC' to 'those bee guys'
+=============
+
+Chrome extension that replaces occurrences of 'CFC' with 'those bee guys'
+
+Installation
+------------
+
+In Chrome, choose Window > Extensions.  Drag CloudToButt.crx into the page that appears.
+
+Sources
+------------
+Shamelssly stole from https://github.com/panicsteve/cloud-to-butt
---
+++ Source/content_script.js
@@ -0,0 +1,47 @@
+walk(document.body);
+
+function walk(node) 
+{
+    // Shamelessly stolen from: https://github.com/panicsteve/cloud-to-butt
+    // I stole this function from here:
+    // http://is.gd/mwZp7E
+    
+    var child, next;
+    
+    if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
+        || node.classList.indexOf('ace_editor') > -1) {
+        return;
+    }
+
+    switch ( node.nodeType )  
+    {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while ( child ) 
+            {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+
+        case 3: // Text node
+            handleText(node);
+            break;
+    }
+}
+
+function handleText(textNode) 
+{
+    var v = textNode.nodeValue;
+
+    v = v.replace(/\bCFC\b/g, "those bee guys");
+    v = v.replace(/\bcfc\b/g, "those bee guys");
+    v = v.replace(/\bthe imperium\b/g, "those bee guys");
+    v = v.replace(/\bThe Imperium\b/g, "those bee guys");
+    v = v.replace(/\bTHE IMPERIUM\b/g, "those bee guys");
+    
+    textNode.nodeValue = v;
+}
